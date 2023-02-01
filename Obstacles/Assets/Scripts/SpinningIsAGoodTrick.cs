8eaa848bb39abde311ee035e08e95125bdf8232a
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2023-02-01 21:47:44 +0200

    spinning is a good trick

---
+++ Obstacles/Assets/Scripts/SpinningIsAGoodTrick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningIsAGoodTrick : MonoBehaviour
+{
+    [SerializeField] private float xValue = 0f;
+    [SerializeField] private float yValue = 0f;
+    [SerializeField] private float zValue = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xValue, yValue, zValue);
+    }
+}
