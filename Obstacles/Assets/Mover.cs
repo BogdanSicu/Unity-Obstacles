a5ee2f637f70abafb1aef29b5f10b819e474759f
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2023-02-01 13:29:00 +0200

    the git finally works

---
+++ Obstacles/Assets/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // [SerializeField] face ca variabila sa poata fi vizibila in Unity
+    // [SerializeField] private float yValue = 0.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // X,Y,Z -> add 1 to X, 0 to Y and 0 to Z axes
+        // transform.Translate(1,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+
+        transform.Translate(xValue,0,zValue);
+    }
+}
